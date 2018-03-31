3f0084842911a130617c6fdd21e0e62eca0381ad
strengthtoendure <francisco.bahamondes90@gmail.com>
2018-03-31 16:07:29 -0300

    Sin jquery ni popper

--- views/footer.php
+++ views/footer.php
@@ -1,8 +1,6 @@
 <footer>
   <p class="copyright">Desarrollado por Francisco Bahamondes - <a href="https://fbcreativos.cl">fbcreativos.cl</a></p>
 </footer>
-<script src="<?php echo RUTA; ?>js/jquery-3.3.1.min.js"></script>
-<script src="<?php echo RUTA; ?>js/popper.min.js"></script>
 <script src="<?php echo RUTA; ?>js/bootstrap.min.js"></script>
 </body>
 </html>
