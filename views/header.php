2b73504aafe8ec573662167ddceaea4ae45c770a
strengthtoendure <francisco.bahamondes90@gmail.com>
2018-03-31 16:06:59 -0300

    Blog más bootstrap

--- views/header.php
+++ views/header.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Blog</title>
+  <link rel="stylesheet" href="<?php echo RUTA; ?>css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald" rel="stylesheet">
   <link rel="stylesheet" href="<?php echo RUTA; ?>css/estilos.css">
